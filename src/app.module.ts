4d00d106f4da6c7eb57f9694054f752aeb6f6b4c
najmulhouda <najmulhouda6@gmail.com>
2024-07-08 01:33:26 +0600

    need to debug when create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest')],
   controllers: [],
   providers: [],
 })
